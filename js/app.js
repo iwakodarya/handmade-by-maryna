250305e8c2e2cdede7019d824686af1cf36561b0
Darya Iwako <darya.iwako@gmail.com>
2024-06-25 13:59:09 -0700

    Adding styling and ability to jump to gallery section.

--- js/app.js
+++ js/app.js
@@ -3,36 +3,67 @@
 const gallery = [
     {
         category_name: "Polymer Clay",
+        id: "polymer-clay",
         folder_name: "01-polymer-clay",
         items: [
             {
-                name: "gold_pendant",
-                desc: "This is a necklace."
+                name: "bead_bracelet",
+                desc: "This is a bracelet."
             },
             {
-                name: "red_brown_necklace",
-                desc: "This is a necklace."
+                name: "barrette1",
+                desc: "This is a barrette."
             },
             {
-                name: "gold_pendant",
-                desc: "This is a necklace. With a very very very very long descripton"
+                name: "yellow_bow",
+                desc: "This is a barrette."
             },
             {
-                name: "red_brown_necklace",
-                desc: "This is a necklace."
+                name: "barrette2",
+                desc: "This is a barrette."
             },
             {
-                name: "gold_pendant",
-                desc: "This is a necklace. With a very very very very long descripton"
+                name: "barrette3",
+                desc: "This is a barrette."
             },
             {
-                name: "red_brown_necklace",
-                desc: "This is a necklace."
+                name: "brown_bow",
+                desc: "This is a barrette."
             },
+
+        ]
+    },
+    {
+        category_name: "Felting",
+        id: "felting",
+        folder_name: "03-felting",
+        items: [
+            {
+                name: "felt_flowers",
+                desc: "Felt flowers."
+            },
+            {
+                name: "felt_mittens",
+                desc: "Felt mittens."
+            },
+            {
+                name: "felt_pumpkin1",
+                desc: "Felt pumpkin."
+            },
+            {
+                name: "felt_pumpkin2",
+                desc: "Felt pumpkin."
+            },
+            {
+                name: "felt_star",
+                desc: "Felt star."
+            },
+        
         ]
     },
     {
         category_name: "Watercolor",
+        id: "watercolor",
         folder_name: "02-watercolor",
         items: [
             {
@@ -52,15 +83,18 @@ function createGalleryHeader(name) {
 
 function createPhotoTile(imagePath, imageDescription) {
     const imgTile = document.createElement('div');
-    // Image
+    // Image (in div container)
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
     const img = document.createElement('img');
     img.setAttribute('src', imagePath);
+    imgContainer.appendChild(img);
     // Desc text
     const imgDesc = document.createElement('p');
     imgDesc.innerHTML = imageDescription;
 
     imgTile.classList.add('gallery-image-tile');
-    imgTile.appendChild(img);
+    imgTile.appendChild(imgContainer);
     imgTile.appendChild(imgDesc);
 
     return imgTile;
@@ -71,12 +105,31 @@ function scrollSectionIntoView(sectionId) {
     clickedSection.scrollIntoView({ behavior: "smooth" });
 };
 
+/* Get element that starts the gallery */
 const galleryDiv = document.getElementById('gallery');
+
+/* Create buttons to jump to gallery sections */
+const galleryJumpButtons = document.createElement('div');
+galleryJumpButtons.id = 'gallery-jump-buttons-container';
+galleryDiv.appendChild(galleryJumpButtons);
 
 gallery.forEach(
     category => {
+        const categoryButton = document.createElement('p');
+        categoryButton.classList.add('gallery-jump-button');
+        categoryButton.innerHTML = category.category_name;
+        categoryButton.id = 'nav--' + category.id;
+        galleryJumpButtons.appendChild(categoryButton);
+    }
+);
+
+/* Create header and gallery for each subsection, eg 'Polymer Clay' */
+gallery.forEach(
+    category => {
         const folderPath = category.folder_name;
-        galleryDiv.appendChild(createGalleryHeader(category.category_name));
+        const galleryHeader = createGalleryHeader(category.category_name);
+        galleryHeader.id = category.id;
+        galleryDiv.appendChild(galleryHeader);
         const galleryGrid = document.createElement('div');
         galleryGrid.classList.add('gallery');
         galleryDiv.appendChild(galleryGrid);
@@ -90,11 +143,20 @@ gallery.forEach(
     }
 );
 
+/* Scroll to section on nav bar click */
 document.getElementById('nav-bar').addEventListener('click',
     event => {
         const sectionId = event.target.id.replace('nav--', '');
         scrollSectionIntoView(sectionId);
     }
 );
+
+/* Scroll to gallery section on click */
+document.getElementById('gallery-jump-buttons-container').addEventListener('click', 
+    event => {
+        const sectionId = event.target.id.replace('nav--', '');
+        scrollSectionIntoView(sectionId);
+    }
+)
 
 
