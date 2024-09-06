79a06c39003b345df37df05304ab1de22716e0b2
Darya Iwako <darya.iwako@gmail.com>
2024-09-06 11:06:29 -0700

    Get photos from Google PHOTOS via API.

--- scripts/upload-photos-to-AWS/uploadToAWS.js
+++ scripts/upload-photos-to-AWS/uploadToAWS.js
@@ -1,9 +1,13 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const fetch = require("node-fetch");
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
 
-// Global object to store googlePhotosData
-const googlePhotosData = [];
+// Amazon AWS S3
+import {
+    S3Client,
+    PutObjectCommand
+} from "@aws-sdk/client-s3";
+
+dotenv.config();
 
 // Authenticate and get Oath2 access token using refresh token
 const getAccessToken = () => {
@@ -41,9 +45,30 @@ const getAlbumPhotos = (albumId, accessToken) => {
             },
             body: JSON.stringify({ 'albumId': albumId })
         }).then(
-            
-        )
+            response => response.json()
+        ).then(
+            result => {
+                if (result.mediaItems) {
+                    return result.mediaItems.map(item => {
+                        return ({
+                            'mediaId': item.id,
+                            'mimeType': item.mimeType,
+                            'baseUrl': item.baseUrl
+                        })
+                    })
+                }
+                else
+                    return []
+            }
+        ).catch(
+            err => console.log('Error in getAlbumPhotos:: ', err)
+        );
+};
 
+const getPhoto = (baseUrl) => {
+    return fetch(baseUrl)
+        .then(response => response.blob())
+        .catch(err => console.log('Error in getPhoto():: ', err))
 };
 
 // Get all albums in Google Photos
@@ -59,26 +84,33 @@ const getAllAlbums = (accessToken) => {
         response => response.json()
     )
         .then(
-            result => {
-                return result.albums.map(album => {
-                    return ({
-                        'album_id': album.id,
-                        'album_name': album.title
-                    })
+            result => result.albums.map(album => {
+                return ({
+                    'album_id': album.id,
+                    'album_name': album.title
                 })
-            }
+            })
         ).catch(err => console.log('Error in getAllAlbums::', err))
 }
 
 // Get Google photos albums and mediaIds from 
 const getPhotosFromGooglePhotosAPI = async () => {
+    // 0) Get access token
     const accessToken = await getAccessToken();
+
     // 1) Get all albums 
-    const photos = await getAllAlbums(accessToken);
-    console.log(photos)
-
-    //for each ablum, fetch mediaIds from that album 
-
+    const albums = await getAllAlbums(accessToken);
+    // 2) Fetch photos for each album
+    for (const a of albums) {
+        const mediaItems = await getAlbumPhotos(a.album_id, accessToken);
+        a['mediaItems'] = mediaItems;
+        for (const item of a['mediaItems']) {
+            const photoBlob = await getPhoto(item.baseUrl);
+            item['blob'] = photoBlob;
+        }
+    }
+    return albums;
 };
+ 
+const photos = await getPhotosFromGooglePhotosAPI();
 
-getPhotosFromGooglePhotosAPI()
