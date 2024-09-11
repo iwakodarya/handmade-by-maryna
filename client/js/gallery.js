f9fd276fc5fe335f2961e1f21dba724596a6c9c1
Darya Iwako <darya.iwako@gmail.com>
2024-09-10 20:08:28 -0700

    Added functions to show back button and started work on show album photos on album click. To be continued.

--- client/js/gallery.js
+++ client/js/gallery.js
@@ -19,14 +19,16 @@ const loadGalleryAlbums = async () => {
         // Get first (and only) photo from designated cover album
         const coverPhotoKeys = await getPhotoKeysInFolder(coverAlbumKey);
         album.coverPhotoSrc = baseS3URL + coverPhotoKeys[0].Key;
-    }
 
-    galleryMenuFolders.forEach((album) => createGalleryAlbum(album));
+        // Create album
+        createGalleryAlbum(album);
+    }
 };
 
 const createGalleryAlbum = (albumInfo) => {
     const albumDiv = document.createElement('div');
     albumDiv.classList.add('gallery-album');
+    albumDiv.id = albumInfo.albumName;
     const albumImg = document.createElement('img');
     albumImg.src = albumInfo.coverPhotoSrc;
     const albumTitle = document.createElement('p');
@@ -38,4 +40,33 @@ const createGalleryAlbum = (albumInfo) => {
     document.getElementById('gallery-menu').appendChild(albumDiv);
 };
 
-document.addEventListener('DOMContentLoaded', loadGalleryAlbums);
+const showBackButton = () => {
+    const backButton = document.createElement('button');
+    backButton.innerHTML =
+        '<i class="fa-solid fa-arrow-left-long"></i>  Back to  albums';
+
+    document.getElementById('gallery-back-button').appendChild(backButton);
+};
+
+const showAlbumPhotos = async (selectedAlbum) => {
+    const albumPhotoKeys = await getPhotoKeysInFolder(selectedAlbum + '/');
+    console.log(albumPhotoKeys);
+    // TO DO: hide menu, show back button, call a function to show each of the photos
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadGalleryAlbums();
+
+    // Behavior on album click
+    // TO DO fix problem when user clicks outside of album (if statement?)
+    document
+        .getElementById('gallery-menu')
+        .addEventListener('click', (event) => {
+            showAlbumPhotos(event.target.parentElement.id);
+        });
+    
+    // Behavior on "Back" button click
+});
+
+
