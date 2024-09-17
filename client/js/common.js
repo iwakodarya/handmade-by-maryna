51eb3a85db9e35ea7b1bf40788645913e752fdcd
Darya Iwako <darya.iwako@gmail.com>
2024-09-16 20:57:55 -0700

    Gallery menu (#2)
    * Adding gallery menu.
    
    * Pulling cover photo from designated S3 folder.
    
    * Minor; console.log removal
    
    * Style changes
    
    * Added functions to show back button and started work on show album photos on album click. To be continued.
    
    * Finishing the rest of gallery functionality.
    
    * Using cached photo keys instead of refetching every time.
    
    * Updated photoKeys to group collage images together into an array.
    
    * Adding gallery album title when album is clicked.
    
    * Adding photo count icon for collages.
    
    * Fixing navigation bar href links and "See full gallery" button

---
+++ client/js/common.js
@@ -0,0 +1,29 @@
+const BUCKET_NAME = 'handmade-by-maryna';
+const BUCKET_URL  = 'https://handmade-by-maryna.s3.us-east-2.amazonaws.com/';
+
+const getPhotoKeysInFolder = async (prefix) => {
+    AWS.config.region = 'us-east-2';
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-2:62f94c93-d03e-4a62-bd05-ecbec4365393'
+    });
+
+    const s3 = new AWS.S3({
+        apiVersion: '2006-03-01',
+        params: {
+            Bucket: 'handmade-by-maryna'
+        }
+    });
+
+    try {
+        const data = await s3
+            .listObjects({
+                Bucket: 'handmade-by-maryna',
+                Prefix: prefix
+            })
+            .promise();
+
+        return data.Contents;
+    } catch (err) {
+        console.log(`Error fetching ${prefix} photos :: `, err);
+    }
+};
---
+++ client/js/gallery.js
@@ -0,0 +1,155 @@
+const galleryData = [
+    {
+        albumName: 'polymer_clay',
+        displayName: 'Polymer Clay'
+    },
+    {
+        albumName: 'felting',
+        displayName: 'Felting'
+    }
+];
+
+const showGalleryMenu = async () => {
+    // Re-set "Gallery" as title
+    document.getElementById('gallery-title').innerHTML = 'Gallery';
+
+    // Get album cover photos
+    for (const album of galleryData) {
+        // If album covers aren't cached yet, fetch them.
+        if (!album.coverPhotoSrc) {
+            // Get first (and only) photo from designated cover album
+            const coverAlbumKey = 'gallery_cover_' + album.albumName + '/';
+            const coverPhotoKeys = await getPhotoKeysInFolder(coverAlbumKey);
+            album.coverPhotoSrc = BUCKET_URL + coverPhotoKeys[0].Key;
+        }
+
+        // Create album
+        createGalleryAlbum(album);
+    }
+};
+
+const createGalleryAlbum = (albumInfo) => {
+    const albumDiv = document.createElement('div');
+    albumDiv.classList.add('gallery-album');
+    albumDiv.id = albumInfo.albumName;
+    const albumImg = document.createElement('img');
+    albumImg.src = albumInfo.coverPhotoSrc;
+    const albumTitle = document.createElement('p');
+    albumTitle.innerHTML = albumInfo.displayName;
+
+    albumDiv.appendChild(albumImg);
+    albumDiv.appendChild(albumTitle);
+
+    document.getElementById('gallery-menu').appendChild(albumDiv);
+};
+
+const showBackButton = () => {
+    const backButton = document.createElement('button');
+    backButton.innerHTML =
+        '<i class="fa-solid fa-arrow-left-long"></i>  Back to  albums';
+
+    document.getElementById('gallery-back-button').appendChild(backButton);
+};
+
+const showGalleryAlbumHeader = (albumName) => {
+    // Update title to include album name
+    document.getElementById('gallery-title').innerHTML = albumName;
+};
+
+const getAlbumPhotosFromS3 = async (selectedAlbum) => {
+    const galleryDataAlbum = galleryData.find(
+        (album) => album.albumName === selectedAlbum
+    );
+    // If photoKeys aren't cached yet, fetch them.
+    if (!galleryDataAlbum.photoKeys) {
+        galleryDataAlbum.photoKeys = await getPhotoKeysInFolder(
+            selectedAlbum + '/'
+        );
+        // group collages into arrays to treat them separately
+        galleryDataAlbum.photoKeys = galleryDataAlbum.photoKeys
+            .map((photo) => {
+                if (photo.Key.includes('/single_')) return photo.Key;
+                else if (photo.Key.match('collage_[a-zA-Z0-9]*_1_')) {
+                    // get name key of this photo
+                    const nameKey = photo.Key.match('collage_[a-zA-Z0-9]*_')[0];
+                    // find other photos with this nameKey
+                    const collagePhotos = galleryDataAlbum.photoKeys
+                        .filter((photo) => photo.Key.includes(nameKey))
+                        .map((photo) => photo.Key);
+                    return collagePhotos.sort();
+                }
+            })
+            // remove remaining photos that are part of a collage
+            .filter((photo) => photo !== undefined);
+    }
+    return galleryDataAlbum;
+};
+
+const showAlbumPhotos = async (selectedAlbum) => {
+    const galleryDataAlbum = await getAlbumPhotosFromS3(selectedAlbum);
+
+    showGalleryAlbumHeader(galleryDataAlbum.displayName);
+
+    for (key of galleryDataAlbum.photoKeys) {
+        const photoElement = document.createElement('div');
+        const photo = document.createElement('img');
+
+        // handle collage images
+        if (typeof key === 'object') {
+            photo.src = BUCKET_URL + key[0];
+            const numPhotosIcon = document.createElement('p');
+            numPhotosIcon.classList.add('photo-count-icon');
+            numPhotosIcon.innerHTML = key.length;
+            photoElement.appendChild(numPhotosIcon);
+        } else photo.src = BUCKET_URL + key;
+
+        // Append to DOM
+        photoElement.appendChild(photo);
+        photoElement.classList.add('gallery-photo');
+        document.getElementById('gallery-photos').appendChild(photoElement);
+    }
+};
+
+const hideAlbumPhotos = () => {
+    document.getElementById('gallery-photos').innerHTML = '';
+};
+
+const hideBackButton = () => {
+    document.getElementById('gallery-back-button').innerHTML = '';
+};
+
+const hideGalleryMenu = () => {
+    document.getElementById('gallery-menu').innerHTML = '';
+};
+
+const hideGalleryAblumTitle = () => {
+    document.getElementById('gallery-title').innerHTML = '';
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    showGalleryMenu();
+
+    // Behavior on album click
+    document
+        .getElementById('gallery-menu')
+        .addEventListener('click', (event) => {
+            if (event.target.id !== 'gallery-menu') {
+                hideGalleryMenu();
+                hideGalleryAblumTitle();
+                showAlbumPhotos(event.target.parentElement.id);
+                showBackButton();
+            }
+        });
+
+    // Behavior on "Back" button click
+    document
+        .getElementById('gallery-back-button')
+        .addEventListener('click', (event) => {
+            if (event.target.nodeName == 'BUTTON') {
+                hideBackButton();
+                hideGalleryAblumTitle();
+                hideAlbumPhotos();
+                showGalleryMenu();
+            }
+        });
+});
